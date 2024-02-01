c759af6947984e74693f3b7857bbd7a68146e9dd
dbanaszak1 <dominikbanaszak530@gmail.com>
2024-02-01 01:22:55 +0100

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Slider from './components/Slider'
+import AboutMe from './components/AboutMe'
+
+const App = () => {
+  return (
+    <>
+    <section className='w-full h-screen'>
+      <AboutMe/>
+      <Slider/>
+      </section>
+    </>
+  )
+}
+
+export default App
