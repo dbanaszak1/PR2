6369a562f4544d86b8065c2931ad0b26ee5a9082
Dominik Banaszak <domban3@st.amu.edu.pl>
2024-06-17 12:35:25 +0200

    add KnowledgeHub Project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,10 +26,76 @@ const Projects = () => {
         image1: 'images/skytravel1.webp',
         image2: 'images/skytravel2.webp',
       }]  
+      const knowledgehub = [
+        {
+          title: 'Knowledge Hub',
+          description: `Hackaton app created in 24 hours with a team of 4 people - W.G & P.Ł & M.Ł. KnowledgeHub 
+          is a comprehensive solution for planning, broadening and testing your knowledge. The application includes 
+          a discussion forum, checking yourself through the test tab and the ability to plan your studies with an interactive AI planner. 
+          Tech: RTT - React-Typescript-Tailwind, REST API, OpenAI API, Python, Flask, Firebase, Github.`,
+          stack: [icons.python , icons.flask, icons.firebase, icons.react, icons.tailwind, icons.ts, icons.restapi, icons.github],
+          image1: 'images/kh1.jpg',
+          image2: 'images/kh2.png',
+          image3: 'images/kh3.png',
+          image4: 'images/kh4.png',
+        }]
   
     return (
     <div className='w-5/6 max-w-[940px] m-auto'>
       <h1 className='font-alkatra text-5xl text-white'>Projects:</h1>
+      <motion.div 
+            initial={{opacity:0, x:'-50%'}}
+            whileInView={{opacity:1, x:'0'}}
+            transition={{duration:1.5}}
+            viewport={{ once: true }}
+            className='relative  md:h-[800px] my-10'
+            > 
+                <div className='py-4 text-white'>
+                    <h2 className='font-alkatra text-3xl bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent'>{knowledgehub[0].title}</h2>
+                    <p className='text-xl'>{knowledgehub[0].description}</p>
+                </div>
+                <div className='md:w-64 text-white flex flex-wrap md:absolute'>
+                  {knowledgehub[0].stack.map((stack, index) => (
+                    <div className='h-10 w-10 m-2' 
+                      key={index}
+                      >
+                        <motion.div
+                          initial={{opacity:0, x:'-50%'}}
+                          whileInView={{opacity:1, x:'0'}}
+                          transition={{delay: index/2,duration:1.5}}
+                          viewport={{ once: true }}
+                        >
+                          {stack}
+                        </motion.div>
+                    </div>
+                  ))}
+                  </div>
+                <img src='images/mac1.png' className='hidden md:block absolute  md:w-[806px] md:h-[420px] z-20 md:-translate-x-[104px] md:translate-y-52'/>    
+                <div className="w-full h-48 md:w-[600px] md:h-[320px] bg-cover z-10 my-6 md:absolute md:translate-y-[216px] rounded-xl shadow-2xl md:shadow-none md:rounded-none shadow-purple-400" style={{backgroundImage: `url(${knowledgehub[0].image1})`}}></div>
+                <div className="w-full shadow-2xl shadow-purple-400 h-48 md:w-[600px] md:h-[320px] md:translate-x-60 md:-translate-y- bg-cover md:absolute border-[1px] border-gray-400 rounded-xl" style={{backgroundImage: `url(${knowledgehub[0].image2})`}}></div>  
+            </motion.div>
+            <div
+              className = "flex place-content-evenly w-full mb-10 md:mb-20 flex-wrap"
+            >
+              <motion.img 
+                src ={knowledgehub[0].image3} 
+                alt = "Knowledge Hub" 
+                className = "shadow-2xl shadow-purple-400 md:h-60 rounded-xl mb-10" 
+                initial={{opacity:0, x:'-50%'}}
+                whileInView={{opacity:1, x:'0'}}
+                transition={{duration:1.5}}
+                viewport={{ once: true }}/>  
+              <motion.img 
+                src ={knowledgehub[0].image4}
+                className = "shadow-2xl shadow-purple-400 md:h-60 rounded-xl" 
+                alt = "Knowledge Hub" 
+                initial={{opacity:0, x:'-50%'}}
+                whileInView={{opacity:1, x:'0'}}
+                transition={{delay: 1, duration:1.5}}
+                viewport={{ once: true }}
+                />             
+            </div>
+
         {project.map((project, index) => (
             <motion.div 
             key={index}
